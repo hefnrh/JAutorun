1f92b5e33901d27ac61c592f3500f63ae0d13506
nibonn <516860685@qq.com>
2014-05-06 17:43:33 +0800

    update main class

--- src/main/java/com/hefnrh/jautorun/Main.java
+++ src/main/java/com/hefnrh/jautorun/Main.java
@@ -3,7 +3,7 @@ package com.hefnrh.jautorun;
 /**
  * Hello world!
  */
-public class App {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
