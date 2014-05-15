9f088f7201d57fa0ca27c815f7834266024a69fb
nibonn <516860685@qq.com>
2014-05-15 22:01:44 +0800

    add all functions

--- src/main/java/com/hefnrh/jautorun/Main.java
+++ src/main/java/com/hefnrh/jautorun/Main.java
@@ -16,17 +16,112 @@ import java.io.IOException;
 
 
 public class Main {
+    static final int USAGE = 0b1;
+    static final int LOGON = 0b10;
+    static final int IEBHO = 0b100;
+    static final int SERVICE = 0b1000;
+    static final int DRIVER = 0b1_0000;
+    static final int TASK = 0b10_0000;
+    static final int BOOT = 0b100_0000;
+    static final int WINSOCK = 0b1000_0000;
+    static final int DLL = 0b1_0000_0000;
+    static final int WINLOGON = 0b10_0000_0000;
+    static final int IMG_HIJACK = 0b100_0000_0000;
+
     public static void main(String[] args) {
-//        printLogon();
-//        printServices();
-//        printIEBHO();
-//        printTaskScheduler();
-//        printDrivers();
-//        printBootExecute();
-        printDLLs();
+        int opt = 0;
+        for (String s : args) {
+            switch (s) {
+                case "logon":
+                    opt |= LOGON;
+                    break;
+                case "iebho":
+                    opt |= IEBHO;
+                    break;
+                case "service":
+                    opt |= SERVICE;
+                    break;
+                case "driver":
+                    opt |= DRIVER;
+                    break;
+                case "task":
+                    opt |= TASK;
+                    break;
+                case "bootexe":
+                    opt |= BOOT;
+                    break;
+                case "winsock":
+                    opt |= WINSOCK;
+                    break;
+                case "dll":
+                    opt |= DLL;
+                    break;
+                case "winlogon":
+                    opt |= WINLOGON;
+                    break;
+                case "img":
+                    opt |= IMG_HIJACK;
+                    break;
+                case "-h":
+                case "--help":
+                case "help:":
+                default:
+                    opt |= USAGE;
+            }
+        }
+        if ((opt & USAGE) != 0 || opt == 0) {
+            usage();
+            return;
+        }
+        if ((opt & LOGON) != 0) {
+            printLogon();
+        }
+        if ((opt & IEBHO) != 0) {
+            printIEBHO();
+        }
+        if ((opt & SERVICE) != 0) {
+            printServices();
+        }
+        if ((opt & DRIVER) != 0) {
+            printDrivers();
+        }
+        if ((opt & TASK) != 0) {
+            printTaskScheduler();
+        }
+        if ((opt & BOOT) != 0) {
+            printBootExecute();
+        }
+        if ((opt & WINSOCK) != 0) {
+            printWinsockProviders();
+        }
+        if ((opt & DLL) != 0) {
+            printDLLs();
+        }
+        if ((opt & WINLOGON) != 0) {
+            printWinlogon();
+        }
+        if ((opt & IMG_HIJACK) != 0) {
+            printImgHijack();
+        }
     }
 
-    public static void printLogon() {
+    static void usage() {
+        StringBuilder sb = new StringBuilder("usage: <options>\n");
+        sb.append("  -h, --help, help  display this help message and exit\n");
+        sb.append("        logon       normal registry logon\n");
+        sb.append("        iebho       internet explorer browser help object\n");
+        sb.append("        service     services\n");
+        sb.append("        driver      drivers\n");
+        sb.append("        task        task scheduler\n");
+        sb.append("        bootexe     boot execute\n");
+        sb.append("        winsock     winsock providers\n");
+        sb.append("        dll         known dlls\n");
+        sb.append("        winlogon    winlogon\n");
+        sb.append("        img         image hijack\n");
+        System.out.println(sb);
+    }
+
+    static void printLogon() {
         printKeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
                 "System\\CurrentControlSet\\Control\\Terminal Server\\Wds\\rdpwd\\StartupPrograms",
                 "HKLM\\System\\CurrentControlSet\\Control\\Terminal Server\\Wds\\rdpwd\\StartupPrograms\t\t\t");
@@ -107,8 +202,8 @@ public class Main {
                 "SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
                 "HKLM\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\RunOnce\t\t\t");
 
-        printDirExe("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup");
-        printDirExe("C:\\Users\\GuYifan\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup");
+        printDirExe(System.getenv("allusersprofile") + "Microsoft\\Windows\\Start Menu\\Programs\\Startup");
+        printDirExe(System.getenv("userprofile") + "AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup");
 
         printKeyValue(RegistryKey.HKEY_CURRENT_USER_INDEX,
                 "Software\\Microsoft\\Windows NT\\CurrentVersion\\Windows\\Load",
@@ -148,55 +243,55 @@ public class Main {
     }
 
     static void printIEBHO() {
-        printKeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+        printDefaultSubkeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
                 "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects",
                 "HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+        printDefaultSubkeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
                 "Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects",
                 "HKLM\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_CURRENT_USER_INDEX,
+        printSubkeyAllValues(RegistryKey.HKEY_CURRENT_USER_INDEX,
                 "Software\\Microsoft\\Internet Explorer\\UrlSearchHooks",
                 "HKCU\\Software\\Microsoft\\Internet Explorer\\UrlSearchHooks\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
                 "HKLM\\Software\\Microsoft\\Internet Explorer\\Toolbar",
                 "HKLM\\Software\\Microsoft\\Internet Explorer\\Toolbar\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
                 "Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Toolbar",
                 "HKLM\\Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Toolbar\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_CURRENT_USER_INDEX,
+        printSubkeyAllValues(RegistryKey.HKEY_CURRENT_USER_INDEX,
                 "Software\\Microsoft\\Internet Explorer\\Explorer Bars",
                 "HKCU\\Software\\Microsoft\\Internet Explorer\\Explorer Bars\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
                 "Software\\Microsoft\\Internet Explorer\\Explorer Bars",
                 "HKLM\\Software\\Microsoft\\Internet Explorer\\Explorer Bars\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_CURRENT_USER_INDEX,
+        printSubkeyAllValues(RegistryKey.HKEY_CURRENT_USER_INDEX,
                 "Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Explorer Bars",
                 "HKCU\\Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Explorer Bars\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
                 "Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Explorer Bars",
                 "HKLM\\Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Explorer Bars\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_CURRENT_USER_INDEX,
+        printSubkeyAllValues(RegistryKey.HKEY_CURRENT_USER_INDEX,
                 "Software\\Microsoft\\Internet Explorer\\Extensions",
                 "HKCU\\Software\\Microsoft\\Internet Explorer\\Extensions\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
                 "Software\\Microsoft\\Internet Explorer\\Extensions",
                 "HKLM\\Software\\Microsoft\\Internet Explorer\\Extensions\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_CURRENT_USER_INDEX,
+        printSubkeyAllValues(RegistryKey.HKEY_CURRENT_USER_INDEX,
                 "Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Extensions",
                 "HKCU\\Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Extensions\t\t\t");
 
-        printKeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
                 "Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Extensions",
                 "HKLM\\Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Extensions\t\t\t");
     }
@@ -237,6 +332,7 @@ public class Main {
     }
 
     static void printTaskScheduler() {
+        System.out.println("task schedulers:\n");
         for (File f : new File("C:\\Windows\\System32\\Tasks").listFiles()) {
             if (f.isDirectory()) {
                 continue;
@@ -281,9 +377,52 @@ public class Main {
     }
 
     static void printWinsockProviders() {
-        printKeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+        printAllSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
                 "System\\CurrentControlSet\\Services\\WinSock2\\Parameters\\Protocol_Catalog9",
                 "HKLM\\System\\CurrentControlSet\\Services\\WinSock2\\Parameters\\Protocol_Catalog9\t\t\t");
+
+    }
+
+    static void printImgHijack() {
+        printAllSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+                "Software\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options",
+                "HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\t\t\t");
+        printAllSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+                "Software\\Microsoft\\Command Processor\\Autorun",
+                "HKLM\\Software\\Microsoft\\Command Processor\\Autorun\t\t\t");
+        printAllSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+                "Software\\Wow6432Node\\Microsoft\\Command Processor\\Autorun",
+                "HKLM\\Software\\Wow6432Node\\Microsoft\\Command Processor\\Autorun\t\t\t");
+        printAllSubkeyAllValues(RegistryKey.HKEY_CURRENT_USER_INDEX,
+                "Software\\Microsoft\\Command Processor\\Autorun",
+                "HKCU\\Software\\Microsoft\\Command Processor\\Autorun\t\t\t");
+        printDefaultSubkeyValue(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+                "SOFTWARE\\Classes\\Exefile\\Shell\\Open",
+                "HKLM\\SOFTWARE\\Classes\\Exefile\\Shell\\Open\\Command\\(Default)\t\t\t");
+    }
+
+    static void printWinlogon() {
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+                "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\Notify",
+                "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\Notify");
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+                "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\System",
+                "HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\System\t\t\t");
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+                "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\UIHost",
+                "HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\UIHost\t\t\t");
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+                "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\GinaDLL",
+                "HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\GinaDLL\t\t\t");
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+                "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\Taskman",
+                "HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\Taskman\t\t\t");
+        printSubkeyAllValues(RegistryKey.HKEY_CURRENT_USER_INDEX,
+                "Control Panel\\Desktop\\Scrnsave.exe",
+                "HKCU\\Control Panel\\Desktop\\Scrnsave.exe\t\t\t");
+        printSubkeyAllValues(RegistryKey.HKEY_LOCAL_MACHINE_INDEX,
+                "System\\CurrentControlSet\\Control\\BootVerificationProgram\\ImageName",
+                "HKLM\\System\\CurrentControlSet\\Control\\BootVerificationProgram\\ImageName\t\t\t");
     }
 
     static void printKeyValue(int index, String subkey, String path) {
@@ -308,7 +447,66 @@ public class Main {
         System.out.println();
     }
 
+    static void printDefaultSubkeyValue(int index, String subkey, String path) {
+        RegistryKey key = RegistryKey.getRootKeyForIndex(index).getSubKey(subkey);
+        System.out.println(path);
+        if (key == null) {
+            int split = subkey.lastIndexOf('\\');
+            String parentKey = subkey.substring(0, split);
+            String name = subkey.substring(split + 1);
+            key = RegistryKey.getRootKeyForIndex(index).getSubKey(parentKey);
+            if (key != null) {
+                RegistryValue value = key.getValue(name);
+                if (value != null) {
+                    System.out.println("\t" + key.getValue(name));
+                }
+            }
+        } else {
+            for (RegistryKey sub : key.getSubKeys()) {
+                System.out.println("\t" + sub.getValue(""));
+            }
+        }
+        System.out.println();
+    }
 
+
+    static void printSubkeyAllValues(int index, String keypath, String path) {
+        RegistryKey key = RegistryKey.getRootKeyForIndex(index).getSubKey(keypath);
+        System.out.println(path);
+        if (key == null) {
+            return;
+        }
+        for (RegistryKey sub : key.getSubKeys()) {
+            System.out.println("\t" + sub.getName());
+            for (RegistryValue v : sub.getValues()) {
+                System.out.println("\t\t" + v);
+            }
+        }
+    }
+
+    static void printAllSubkeyAllValues(int depth, RegistryKey root) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < depth; ++i) {
+            sb.append('\t');
+        }
+        System.out.println(sb.toString() + root);
+        sb.append('\t');
+        for (RegistryValue v : root.getValues()) {
+            System.out.println(sb.toString() + v);
+        }
+        System.out.println();
+        for (RegistryKey k : root.getSubKeys()) {
+            printAllSubkeyAllValues(depth + 1, k);
+        }
+    }
+
+    static void printAllSubkeyAllValues(int index, String keypath, String path) {
+        RegistryKey key = RegistryKey.getRootKeyForIndex(index).getSubKey(keypath);
+        System.out.println(path);
+        if (key != null) {
+            printAllSubkeyAllValues(1, key);
+        }
+    }
     static void printDirExe(String path) {
         File dir = new File(path);
         System.out.println(path);
